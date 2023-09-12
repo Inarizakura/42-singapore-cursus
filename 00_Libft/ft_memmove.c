b3c83cd37b7ff55cd50f410e85c92be27554940e
Fushimi-Kitsune <itsukaryuuji@gmail.com>
2023-09-12 17:55:10 +0800

    Committing current progress

--- 00_Libft/ft_memmove.c
+++ 00_Libft/ft_memmove.c
@@ -6,13 +6,36 @@
 /*   By: dphang <dphang@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/06 15:09:57 by dphang            #+#    #+#             */
-/*   Updated: 2023/09/09 15:17:04 by dphang           ###   ########.fr       */
+/*   Updated: 2023/09/08 06:53:27 by dphang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*memmove(void *dest, const void *src, size_t n)
+void	*ft_memmove(void *dest, const void *src, size_t n)
 {
+	size_t	i;
+	char	*d;
+	char	*s;
+
+	i = 0;
+	d = (char *)dest;
+	s = (char *)src;
+	if (&dest[i] < &src[i])
+	{
+		while (i < n)
+		{
+			d[i] = s[i];
+			i++;
+		}
+	}
+	else
+	{
+		while (n > 0)
+		{
+			d[n - 1] = s[n - 1];
+			n--;
+		}
+	}
 	return (dest);
 }
