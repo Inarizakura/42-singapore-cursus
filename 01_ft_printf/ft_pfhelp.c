b0759a84fea3c6b25c786650b7ccc07b687e8713
Dillon Phang <dphang@c2r4s8.42singapore.sg>
2023-09-23 18:55:41 +0800

    Committing current progress

--- 01_ft_printf/ft_pfhelp.c
+++ 01_ft_printf/ft_pfhelp.c
@@ -6,7 +6,7 @@
 /*   By: dphang <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/22 14:53:35 by dphang            #+#    #+#             */
-/*   Updated: 2023/09/22 15:58:09 by dphang           ###   ########.fr       */
+/*   Updated: 2023/09/23 18:53:32 by dphang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,7 @@
 
 int	ft_putchar(char c)
 {
-	write(1, &c, 1);
-	return (1);
+	return (write(1, &c, 1));
 }
 
 int	ft_putstr(char *s)
@@ -23,10 +22,29 @@ int	ft_putstr(char *s)
 	int i;
 
 	i = 0;
+	if (!s)
+		return (0);
 	while (s[i])
-	{
-		write(1, &s[i], 1);
-		i++;
-	}
+		i += write(1, &s[i], 1);
 	return (i);
+}
+
+int	ft_putnbrbase(int nbr)
+{
+	int			count;
+	long int	nb;
+
+	count = 0;
+	nb = nbr;
+	if (nbr < 0)
+	{
+		ft_putchar('-');
+		nbr *= -1;
+		count++;
+	}
+	if (nbr > 9)
+		ft_putnbrbase(nbr / 10);
+	if (nbr >= 0)
+		count += ft_putchar((nbr % 10) + '0');
+	return (count);
 }
