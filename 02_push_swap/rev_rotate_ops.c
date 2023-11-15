ff20068eaf0382488ea1a2c6e5f270226d1dd20a
Inarizakura <dillondayo@gmail.com>
2023-11-15 15:00:33 +0800

    Initial commit

---
+++ 02_push_swap/rev_rotate_ops.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rev_rotate_ops.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dphang <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/15 14:11:12 by dphang            #+#    #+#             */
+/*   Updated: 2023/11/15 14:12:35 by dphang           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+	rra - reverse rotate a: Shift down all elements of stack a by 1.
+
+	rrb - reverse rotate b: Shift down all elements of stack b by 1.
+
+	The last element becomes the first one.
+
+	rrr: rra and rrb at the same time.
+*/
+
+void	rra();
+
+void	rrb();
+
+void	rrr();
---
+++ 02_push_swap/rotate_ops.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rotate_ops.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dphang <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/15 14:08:59 by dphang            #+#    #+#             */
+/*   Updated: 2023/11/15 14:11:01 by dphang           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+	ra - rotate a: Shift up all elements of stack a by 1. 
+
+	rb - rotate b: Shift up all elements of stack b by 1.
+
+	The first element becomes the last one.
+
+	rr: ra and rb at the same time.
+*/
+
+void	ra();
+
+void	rb();
+
+void	rr();
